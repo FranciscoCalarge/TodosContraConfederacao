d772a4d3aa0eb82fbcf200f79d9949a0ce9b66a3
Francisco José Costa Calarge <54690356+FranciscoCalarge@users.noreply.github.com>
2024-09-26 02:43:46 -0400

    primeira noite de trabalho
    implementada uma UI rústica com cards selecionaveis, combate ainda inexistente

---
+++ Assets/I_Interactable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface I_Interactable
+{
+    public Vector3 position { get; set; }
+}
---
+++ Assets/PlayerHandScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+
+public class PlayerHandScript : MonoBehaviour
+{
+    public GameObject CardPrefab;
+    public GameObject CursorPrefab;
+    public Transform handTransform;
+    public int maxCards = 5;
+    public List<GameObject> cards = new List<GameObject>();
+
+
+    private Transform _currentActiveObject;
+    private Transform _cursorObj;
+    private int _cursorIndex;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < maxCards; i++)
+        {
+            GameObject card =  Instantiate(CardPrefab, handTransform.position,Quaternion.identity, handTransform);
+            card.transform.position = new Vector3(Mathf.InverseLerp(0, maxCards, i) * maxCards - maxCards / 2, 0, 0)+transform.position;
+            cards.Add(card);
+            
+        }
+        _currentActiveObject = cards[0].transform;
+        _cursorObj = Instantiate(CursorPrefab).transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _cursorObj.position = _currentActiveObject.position+Vector3.up+new Vector3(0,Mathf.Sin(Time.time*5)*.1f,0);
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _currentActiveObject = cards[(int)Mathf.Repeat(++_cursorIndex,cards.Count)].transform;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _currentActiveObject=cards[(int)Mathf.Repeat(--_cursorIndex, cards.Count)].transform;
+        }
+    }
+}
